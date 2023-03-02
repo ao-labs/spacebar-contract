4a4de444aa9257ce50a79192f67141ac50fced0d
pluto <pluto@ao.xyz>
2023-03-02 11:45:49 +0900

    Set Arbitrum and Optimism's network RPC Urls

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,29 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import * as dotenv from "dotenv"
+
+dotenv.config()
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+	solidity: "0.8.17",
+	networks: {
+		arbitrum: {
+			url: `https://arb1.arbitrum.io/rpc`,
+			accounts: [process.env.ARBITRUM_MAINNET_ACCOUNT || ""],
+		},
+		optimism: {
+			url: `https://mainnet.optimism.io`,
+			accounts: [process.env.OPTIMISM_MAINNET_ACCOUNT || ""],
+		},
+		"arbitrum-goerli": {
+			url: `https://goerli-rollup.arbitrum.io/rpc	`,
+			accounts: [process.env.ARBITRUM_GOERLI_ACCOUNT || ""],
+		},
+		"optimism-goerli": {
+			url: `https://goerli.optimism.io`,
+			accounts: [process.env.OPTIMISM_GOERLI_ACCOUNT || ""],
+		},
+	},
+}
 
-export default config;
+export default config
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,31 @@
-import { ethers } from "hardhat";
+import { ethers } from "hardhat"
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+	const [deployer] = await ethers.getSigners()
 
-  const lockedAmount = ethers.utils.parseEther("1");
+	console.log("Deploying contracts with the account:", deployer.address)
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+	console.log("Account balance:", (await deployer.getBalance()).toString())
 
-  await lock.deployed();
+	const currentTimestampInSeconds = Math.round(Date.now() / 1000)
+	const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60
+	const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+	const lockedAmount = ethers.utils.parseEther("0")
+
+	const Lock = await ethers.getContractFactory("Lock")
+	const lock = await Lock.deploy(unlockTime, { value: lockedAmount })
+
+	await lock.deployed()
+
+	console.log(
+		`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+	)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+	console.error(error)
+	process.exitCode = 1
+})
