1342a77e2fb24cf73e256c61f9639db26c6a55fe
pluto <pluto@ao.xyz>
2023-07-10 14:40:10 +0900

    update 6551 account contract and move 6551 contracts under ERC6551 directory

--- test/BadgeSBTUniverse1.t.sol
+++ test/BadgeSBTUniverse1.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
-import "../contracts/ERC6551Registry.sol";
+import "../contracts/ERC6551/ERC6551Registry.sol";
 import "../contracts/SpaceFactoryV1.sol";
 import "../contracts/BadgeSBTUniverse1.sol";
 import "../contracts/SpaceshipNFTUniverse1.sol";
