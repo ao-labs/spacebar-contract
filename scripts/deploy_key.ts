12c7e2d4855e76b816bcdce56b63a3c0a9cad821
pluto <pluto@ao.xyz>
2023-12-12 12:33:11 +0900

    update Key deploy code

--- scripts/deploy_key.ts
+++ scripts/deploy_key.ts
@@ -1,55 +1,61 @@
-import { ethers } from "hardhat"
-import { KeyMinterUniverse1 } from "../typechain-types"
+import { ethers, upgrades } from "hardhat"
+import { KeyMinterV1, KeyUniverse1 } from "../typechain-types"
 
 async function main() {
 	const runVerify = true
-	/// @dev Deploying KeyMinterUniverse1
-	console.log("Deploying KeyMinterUniverse1...")
-	const KeyMinterUniverse1 = await ethers.getContractFactory(
-		"KeyMinterUniverse1"
-	)
+	/// @dev Deploying KeyMinterV1
+	console.log("Deploying KeyMinterV1...")
+	const KeyMinterV1 = await ethers.getContractFactory("KeyMinterV1")
 
-	const keyMinterUniverse1 = (await KeyMinterUniverse1.deploy(
+	const keyMinterV1 = (await upgrades.deployProxy(KeyMinterV1, [], {
+		initializer: false,
+	})) as KeyMinterV1
+
+	await keyMinterV1.deployed()
+
+	console.log("KeyMinterV1 is deployed to:", keyMinterV1.address)
+
+	/// @dev Deploying KeyUniverse1
+	console.log("Deploying KeyUniverse1...")
+	const KeyUniverse1 = await ethers.getContractFactory("KeyUniverse1")
+
+	const keyUniverse1 = (await KeyUniverse1.deploy(
+		process.env.DEFAULT_ADMIN_ADDRESS,
+		process.env.OPERATOR_ADDRESS,
+		keyMinterV1.address
+	)) as KeyUniverse1
+
+	await keyUniverse1.deployed()
+
+	console.log("KeyUniverse1 is deployed to:", keyUniverse1.address)
+
+	console.log("Intializing KeyMinterV1...")
+	await keyMinterV1.initialize(
+		// @ts-ignore
+		process.env.VAULT_ADDRESS,
 		process.env.DEFAULT_ADMIN_ADDRESS,
 		process.env.OPERATOR_ADDRESS,
 		process.env.SERVICE_ADMIN_ADDRESS,
-		process.env.SPACESHIP_NFT_ADDRESS,
+		process.env.SPACEBAR_UNIVERSE1_ADDRESS,
+		keyUniverse1.address,
 		process.env.TBA_REGISTRY_ADDRESS,
 		process.env.TBA_IMPLEMENTATION_ADDRESS
-	)) as KeyMinterUniverse1
-
-	await keyMinterUniverse1.deployed()
-
-	console.log(
-		"KeyMinterUniverse1 is deployed to:",
-		keyMinterUniverse1.address
 	)
-
-	const keyUniverse1 = await keyMinterUniverse1.keyUniverse1()
-	await keyMinterUniverse1.keyUniverse1()
-	console.log("KeyUniverse1 is deployed to:", keyUniverse1)
 
 	if (runVerify) {
 		console.log("----------------Verification---------------")
 		console.log("Waiting for 5 block confirmations...")
 		const WAIT_BLOCK_CONFIRMATIONS = 5
+		await keyMinterV1.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS)
 
-		await keyMinterUniverse1.deployTransaction.wait(
-			WAIT_BLOCK_CONFIRMATIONS
-		)
-		console.log("Verifying KeyMinterUniverse1 on etherscan...")
+		console.log("Verifying KeyMinterV1 implementation on etherscan...")
+		const implementationAddress =
+			await upgrades.erc1967.getImplementationAddress(keyMinterV1.address)
 		// @ts-ignore
 		await run(`verify:verify`, {
-			address: keyMinterUniverse1.address,
-			constructorArguments: [
-				process.env.DEFAULT_ADMIN_ADDRESS,
-				process.env.OPERATOR_ADDRESS,
-				process.env.SERVICE_ADMIN_ADDRESS,
-				process.env.SPACESHIP_NFT_ADDRESS,
-				process.env.TBA_REGISTRY_ADDRESS,
-				process.env.TBA_IMPLEMENTATION_ADDRESS,
-			],
+			address: implementationAddress,
 		})
+
 		console.log("Verifying KeyUniverse1 on etherscan...")
 		// @ts-ignore
 		await run(`verify:verify`, {
@@ -57,7 +63,7 @@ async function main() {
 			constructorArguments: [
 				process.env.DEFAULT_ADMIN_ADDRESS,
 				process.env.OPERATOR_ADDRESS,
-				keyMinterUniverse1.address,
+				keyMinterV1.address,
 			],
 		})
 	}
